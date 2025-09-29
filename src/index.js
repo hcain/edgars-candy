512c410f84f6564ba2073261d3c29650e1f1c70c
hcain <hcain23@gmail.com>
2025-09-29 16:36:53 -0400

    debugging express on next deployment

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import users from './db/users.json' with { type: "json" };
 
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
-app.use(express.static('dist'));
+// app.use(express.static('dist'));
+app.get('/', (req, res) => res.sendFile('index.html', { root: path.join(__dirname, '../dist') }))
 app.get('/api/getUsers', (req, res) => res.send({ users }));
 
 app.listen(process.env.PORT || 8080, () =>
