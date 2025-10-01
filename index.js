45e002b68bbb84b320984941b68938a862c985af
hcain <hcain23@gmail.com>
2025-10-01 16:10:20 -0400

    moved sever file to continue troubleshooting vercel deployment

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import users from './src/db/users.json' with { type: "json" };
+
+
+const app = express();
+
+// vercel automatically serves static assets
+// app.use(express.static('dist'));
+app.get('/api/getUsers', (req, res) => res.send({ users }));
+
+app.listen(8080, () =>
+  console.log(`Listening on port 8080!`)
+);
